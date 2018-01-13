f99099f7eee10cc549d53c11d9f32ae103a1b533
Sitdhibong Laokok <sitdhibong@gmail.com>
2018-01-14 00:50:57 +0700

    Init project

---
+++ Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Sitdhibong Laokok on 6/1/61.
+//  Copyright © พ.ศ. 2561 Apps Tree. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
+    
+}
+
